416487be34537093f4122b419bf01bcd2b3fab28
JossDav0126 <jossdavidguzmanc026@gmail.com>
2025-08-13 13:41:21 -0600

    Mejoras de aprendizaje

--- bin/dart_basics2.dart
+++ bin/dart_basics2.dart
@@ -1,5 +1,39 @@
-import 'package:dart_basics2/dart_basics2.dart' as dart_basics2;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_basics2.calculate()}!');
+import 'package:dart_basics2/dart_basics2.dart' as dart_basics2;
+void main(List<String> arguments) {/*
+
+  print ('Introduce tu año de nacimiento: ');
+  String fecha = stdin.readLineSync()!;
+  const int fechaActual = 2025;
+  int fotmatDate = int.parse(fecha);
+  int edad = fechaActual - fotmatDate;
+  print('Tu edad es: $edad');
+
+  print(dart_basics2.verificarEdad(edad));
+
+  print('¿Cuanto tiempo llevas programando?');
+  String tiempo = stdin.readLineSync()!;
+
+  print(dart_basics.tipoProgramador(int.parse(tiempo)));
+
+  print('Cual día de la semana te gusta mas, del 1 al 7?, (ej. Lunes es 1)');
+  String dia = stdin.readLineSync()!;
+  print(dart_basics2.diaSemana(int.parse(dia)));
+
+  print ('Hola, escribe un mes del 1 al 12:');
+  String mes = stdin.readLineSync()!;
+  print(dart_basics2.mesAnio(int.parse(mes)));*/
+
+  print ('Introduce un numero para hacer una piramide de asteriscos:');
+  String numero = stdin.readLineSync()!;
+  dart_basics2.piramide(int.parse(numero));
+
+  print ('Introduce un numero para hacer una piramide invertida de asteriscos:');
+  String numero2 = stdin.readLineSync()!;
+  dart_basics2.piramideInvertida(int.parse(numero2));
+
+  print ('Introduce un numero para hacer una piramide espaciada:');
+  String numero3 = stdin.readLineSync()!;
+  dart_basics2.piramideCompleta(int.parse(numero3));
 }
--- lib/dart_basics2.dart
+++ lib/dart_basics2.dart
@@ -1,3 +1,118 @@
+import 'dart:io';
+
 int calculate() {
   return 6 * 7;
 }
+
+String verificarEdad(int edad) {
+  if (edad >= 18) {
+    return 'Eres mayor de edad';
+  } else {
+    return 'Eres menor de edad';
+  }
+}
+
+String tipoProgramador(int tiempo) {
+  if (tiempo < 5) {
+    return 'Eres un programador Junior';
+  } else if (tiempo >= 5 && tiempo < 8) {
+    return 'Eres un programador MID';
+  } else {
+    return 'Eres un programador SENIOR';
+  }
+}
+
+String diaSemana(int dia) {
+  if (dia > 1 && dia < 7) {
+    switch (dia) {
+      case 1:
+        return 'Lunes';
+      case 2:
+        return 'Martes';
+      case 3:
+        return 'Miércoles';
+      case 4:
+        return 'Jueves';
+      case 5:
+        return 'Viernes';
+      case 6:
+        return 'Sábado';
+      case 7:
+        return 'Domingo';
+      default:
+        return 'Día inválido';
+    }
+  } else {
+    print('Ingresa un dia valido: ');
+    dia = int.parse(stdin.readLineSync()!);
+    return diaSemana(dia);
+  }
+}
+
+String mesAnio(int mes) {
+  if (mes > 1 && mes < 13) {
+    switch (mes) {
+      case 1:
+        return 'Enero';
+      case 2:
+        return 'Febrero';
+      case 3:
+        return 'Marzo';
+      case 4:
+        return 'Abril';
+      case 5:
+        return 'Mayo';
+      case 6:
+        return 'Junio';
+      case 7:
+        return 'Julio';
+      case 8:
+        return 'Agosto';
+      case 9:
+        return 'Septiembre';
+      case 10:
+        return 'Octubre';
+      case 11:
+        return 'Noviembre';
+      case 12:
+        return 'Diciembre';
+      default:
+        return 'Mes inválido';
+    }
+  } else {
+    print('Ingresa un mes valido: ');
+    mes = int.parse(stdin.readLineSync()!);
+    return mesAnio(mes);
+  }
+}
+
+void piramide(int numero) {
+  for (int i = 1; i <= numero; i++) {
+    for (int j = 1; j <= i; j++) {
+      stdout.write('*');
+    }
+    print('');
+  }
+}
+
+void piramideInvertida(int numero) {
+  for (int i = numero; i >= 1; i--) {
+    for (int j = 1; j <= i; j++) {
+      stdout.write('*');
+    }
+    print('');
+  }
+}
+
+void piramideCompleta(int numero) {
+  for (int i = 1; i <= numero; i++) {
+    for (int j = 1; j <= numero - i; j++) {
+      stdout.write(' ');
+    }
+    for (int k = 1; k <= (2 * i - 1); k++) {
+      stdout.write('*');
+    }
+    print('');
+  }
+}
+// lib/dart_basics.dart
